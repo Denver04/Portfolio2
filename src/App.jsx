e89ef8e2cad0ddc5d5a823bed059068deab3cdb3
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-01-24 05:48:38 +0530

    width change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <hr></hr>
       <Body />
     </div>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,7 +8,32 @@ function Body() {
         <div className='body-left'>
             <h4>Hii , I'm Abhisek</h4>
             <h5>Front-end developer , Web Designer and a C++ programmer</h5>
+            <h6>Currently, I'm persuing B.E in Army Institute of Technology , Pune from Computer Science branch.</h6>
+            <div className='icons'>
+              <div className='tooltip'>
+                <a href="https://www.instagram.com/denver_d_deniver/" target="_blank" className='insta'><i class="fa-brands fa-instagram-square" ></i></a>
+                <span class="tooltiptext">Instagram</span>
+              </div>
+
+              <div className='tooltip'>
+                <a href="https://www.facebook.com/profile.php?id=100009848738409" target="_blank" className='fb'><i class="fa-brands fa-facebook-square" ></i></a>
+                <span class="tooltiptext">facebook</span>
+              </div>
+
+              <div className='tooltip'>
+                <a href="https://github.com/Denver04" target="_blank" className='github'><i class="fa-brands fa-github-square" ></i></a>
+                <span class="tooltiptext">github</span>
+              </div>
+
+              <div className='tooltip'>
+                <a href="https://www.linkedin.com/in/denver-abhisek-088592217/" target="_blank" className='linkedin'><i class="fa-brands fa-linkedin" ></i></a>
+                <span class="tooltiptext">linkedin</span>
+              </div>
+              
+              {/* <i class="fa-brands fa-discord"></i> */}
+            </div>
         </div>
+
         <div className="body-right">      
             <img className='image' src={main} alt="photo" />
         </div>
