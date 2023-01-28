0e96ac472602bf05ce44cf8c9380b86f2c5ae845
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-01-29 05:29:55 +0530

    active btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,26 @@ import Resume from "../images/RESUME.pdf"
 
 function Navbar() {
   return (
+    
+    var header = document.getElementById("head-elments");
+    var btns = header.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+      });
+    }
+    
     <div className='header'>
         <div className='head-part1'>
             <div className='head-elments'>
-                <Link className='a' to="/">Home</Link>
+                <Link className='a btn active' to="/">Home</Link>
                 {/* <a className='a' href='#project'>Projects</a>
                 <a className='a' href='#education'>Education</a>
                 <a className='a' href='#contact'>Contact</a> */}
-                <Link className='a' to="/skill">Skill</Link>
-                <Link className='a' to="/project">Project</Link>
+                <Link className='a btn' to="/skill">Skill</Link>
+                <Link className='a btn' to="/project">Project</Link>
                 {/* <Link className='a' to="/contact">Contact</Link> */}
                 <a className="a" href={Resume} download>Download Resume</a>
             </div>
