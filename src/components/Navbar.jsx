51452eb4fc453fdc6165ee934bab15c366fbf616
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-01-29 05:39:17 +0530

    s added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ function Navbar() {
                 {/* <a className='a' href='#project'>Projects</a>
                 <a className='a' href='#education'>Education</a>
                 <a className='a' href='#contact'>Contact</a> */}
-                <Link className='a btn' to="/skill">Skill</Link>
-                <Link className='a btn' to="/project">Project</Link>
+                <Link className='a btn' to="/skill">Skills</Link>
+                <Link className='a btn' to="/project">Projects</Link>
                 {/* <Link className='a' to="/contact">Contact</Link> */}
                 <a className="a" href={Resume} download>Download Resume</a>
             </div>
